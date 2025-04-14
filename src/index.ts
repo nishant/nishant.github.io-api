79cd3d7d138293732bb7fa0b18041f39b5f3a57f
nishant <nish54@gmail.com>
2025-04-14 01:01:29 -0400

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 import weatherRouter from './routes/weather';
@@ -6,6 +7,21 @@ import weatherRouter from './routes/weather';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const allowedOrigins = [
+  'http://localhost:4200',
+  'https://nishant.github.io'
+];
+
+app.use(cors({
+  origin: (origin: any, callback: any) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
 app.use('/api/weather', weatherRouter);
 
