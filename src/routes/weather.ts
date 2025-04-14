daab36fb43e3673f0e34552e2944a5926951c969
nishant <nish54@gmail.com>
2025-04-14 03:03:41 -0400

    weather location

--- src/routes/weather.ts
+++ src/routes/weather.ts
@@ -3,9 +3,10 @@ import axios from 'axios';
 
 const router = express.Router();
 const API_KEY = process.env.OPENWEATHERMAP_API_KEY;
-const BASE_URL = 'https://api.openweathermap.org/data/3.0/onecall';
+const DATA_BASE_URL = 'https://api.openweathermap.org/data/3.0/onecall';
+const LOCATION_BASE_URL = 'https://api.openweathermap.org/geo/1.0/reverse';
 
-router.get('/', async (req: Request, res: Response) => {
+router.get('/data', async (req: Request, res: Response) => {
   const {lat, lon} = req.query;
   
   if (!lat || !lon) {
@@ -14,7 +15,7 @@ router.get('/', async (req: Request, res: Response) => {
   }
   
   try {
-    const response = await axios.get(BASE_URL, {
+    const response = await axios.get(DATA_BASE_URL, {
       params: {
         lat,
         lon,
@@ -33,6 +34,46 @@ router.get('/', async (req: Request, res: Response) => {
   } catch (error: any) {
     res.status(500).json({
       error: 'Failed to fetch weather data',
+      details: error.message
+    });
+  }
+});
+
+router.get('/location', async (req: Request, res: Response) => {
+  const {lat, lon} = req.query;
+  
+  if (!lat || !lon) {
+    res.status(400).json({error: 'Latitude (lat) and Longitude (lon) are required'});
+    return;
+  }
+  
+  try {
+    const response = await axios.get(LOCATION_BASE_URL, {
+      params: {
+        lat,
+        lon,
+        limit: 1,
+        appid: API_KEY
+      }
+    });
+    const location = response.data[0];
+    
+    if (!location) {
+      res.status(404).json({ error: 'Location not found' });
+      return;
+    }
+    
+    res.json({
+      city: location.name,
+      state: location.state,
+      country: location.country,
+      lat: location.lat,
+      lon: location.lon
+    });
+    
+  } catch (error: any) {
+    res.status(500).json({
+      error: 'Failed to perform reverse geocoding',
       details: error.message
     });
   }
